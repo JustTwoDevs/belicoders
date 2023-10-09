313bfe688e67c958c93a8b31eaf11136a2ef8d49
carlos paez <carlosa.paezc@autonoma.edu.co>
2023-10-09 15:10:39 -0500

    gui changes

--- src/components/DevTeam.jsx
+++ src/components/DevTeam.jsx
@@ -3,8 +3,8 @@ import DevTeamCard from "./DevTeamCard";
 export default function DevTeam() {
   return (
     <section className="flex flex-col items-center w-full gap-8 p-10">
-      <h1 className="text-white text-4xl">Dev Team</h1>
-      <section className="grid grid-cols-1 gap-10 xl:grid-cols-3">
+      <h1 className="text-center text-white text-3xl xl:text-5xl">Dev Team</h1>
+      <section className="grid grid-cols-1 gap-10 xl:grid-cols-3 xl:gap-16">
         <DevTeamCard name="Carlos" imgsrc="random" />
         <DevTeamCard name="Valentina" imgsrc="random" />
         <DevTeamCard name="Tomas" imgsrc="random" />
