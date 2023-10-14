8f01c5bc003ed3a2e14b1f5430ce5ab854842be7
carlos paez <carlosa.paezc@autonoma.edu.co>
2023-10-14 09:23:50 -0500

    verifyCode change component

--- src/app/verifyRecoveryCode/[id]/page.jsx
+++
@@ -1,9 +0,0 @@
-import VerifyCodeC from "@/components/VerifyCodeC";
-
-export default function VerifyRecoveryCode({ params }) {
-  return (
-    <main className="bg-[#241f35] text-white flex justify-center">
-      <VerifyCodeC userId={params.id} />
-    </main>
-  );
-}
--- src/app/verifyRecoveryCode/[userId]/page.jsx
+++ src/app/verifyRecoveryCode/[userId]/page.jsx
@@ -1,51 +1,54 @@
 "use client";
 import Form from "@/components/Form";
-import key from "@/assets/key.png";
+import verify from "@/assets/verify.png";
 
-export default function ForgotPassword() {
+export default function VerifyCode({ params }) {
   const onSubmit = async (e) => {
     e.preventDefault();
-    const userInfo = e.target.userInfo.value;
+    const verifyCode = e.target.verifyCode.value;
+    console.log(verifyCode);
 
     try {
       const response = await fetch(
-        "http://localhost:3000/api/v1/recoveryCodes",
+        "http://localhost:3000/api/v1/verifyRecoveryCode",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ userInfo }),
+          credentials: "include",
+          body: JSON.stringify({ userId: params.userId, verifyCode }),
         },
       );
 
       const data = await response.json();
       if (response.ok) {
         console.log(data);
-        alert("Código de recuperación enviado a tu correo electronico");
-        router.push(`/verifyRecoveryCode/${data.userId}`);
+        alert("Código de recuperación correcto");
+        router.push(`/resetPassword`);
       } else {
         alert(data.message);
       }
     } catch (error) {
-      console.log(`Error al generar código de recuperación: ${error.message}`);
+      console.log(
+        `Error al verificar el código de recuperación: ${error.message}`,
+      );
     }
   };
 
   return (
     <main className="bg-white h-screen flex flex-col justify-center items-center gap-4">
       <Form
-        image={key}
-        alt="key"
-        title="¿Do you have problems to login?"
-        description="Enter your email or username and we&rsquo;ll send you a link to log
-        back into your account."
+        image={verify}
+        alt="verify"
+        title="¿Have you received your recovery code?"
+        description="Enter your verification Code"
         inputs={[
           {
             type: "text",
-            name: "userInfo",
-            placeholder: "Username or Email",
+            name: "verifyCode",
+            placeholder: "Enter verification code",
           },
         ]}
-        buttonText="Send Recovery Code"
+        buttonText="Verify code"
         link1="/login"
         linkText1="Sign In"
         text2="Don't have an account yet?"
