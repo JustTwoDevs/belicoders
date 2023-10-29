ffc6b581fb462aac21326fed13312ce67b9661ba
ValenMorales <valentina.moraless@autonoma.edu.co>
2023-10-28 19:59:28 -0500

    selector rival type

--- src/app/createRival/page.jsx
+++ src/app/createRival/page.jsx
@@ -1,17 +1,23 @@
+'use client';
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { InputNumber } from "primereact/inputnumber";
 import { FileUpload } from "primereact/fileupload";
+import { SelectButton } from "primereact/selectbutton";
 import EditorComponent from "@/components/EditorComponent";
 import Tags from "@/components/Tags";
 import CodeEditor from "@/components/CodeEditor";
+import { useState } from "react";
 
 export default function CreateRival() {
+  const[rivalKind, setRivalKind] = useState("Algorithm");
   return (
-    <main className="flex flex-col py-8 px-4 min-h-screen w-full">
-      <section className="flex md:flex-col lg:flex-row gap-5 w-full h-[82vh]">
+    <main className="flex flex-col  py-8 px-4 min-h-screen ">
+      <section className="flex flex-col lg:flex-row gap-5 w-full h-[82vh]">
         <section className="w-full h-full border border-solid border-gray-300 rounded">
           <form className="flex flex-col gap-10 p-7">
+          <SelectButton value={rivalKind} onChange={(e) => setRivalKind(e.rivalKind)}
+          options={["Algorithm", "sql"]}  />
             <InputText placeholder="Title" className="border border-gray-200" />
             <EditorComponent
               className="border border-gray-200"
@@ -40,7 +46,7 @@ export default function CreateRival() {
           </form>
         </section>
       </section>
-      <section className="flex gap-5 p-5 md:justify-end lg:justify-end w-full h-full">
+      <section className="flex gap-5 p-5 justify-end w-full h-full">
         <Button
           className="w-32 h-12 bg-primary-200 p-2"
           label="Save draft"
