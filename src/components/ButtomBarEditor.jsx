b94d6da06cffa3da9f28c3eac013ea40c50d5cdc
ttrejosg <tomtrejosgil@gmail.com>
2023-11-04 17:21:57 -0500

    Run code

--- src/components/ButtomBarEditor.jsx
+++ src/components/ButtomBarEditor.jsx
@@ -1,4 +1,58 @@
 export default function ButtomBarEditor(props) {
+  async function runCode() {
+    const url = `http://localhost:3000/api/v1/runCode/${
+      props.type == "AlgorithmRival" ? "algorithm" : "SQL"
+    }`;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify(
+          props.type == "AlgorithmRival"
+            ? { ...props.algorithm, userCode: props.userCode.getValue() }
+            : { ...props.sql, userCode: props.userCode.getValue() }
+        ),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        props.setOutput(data);
+        if (!props.openConsole) props.setOpenConsole(true);
+        props.changeToConsole();
+      } else console.log(data);
+    } catch (error) {
+      console.log(`Error al correr el codigo: ${error.message}`);
+    }
+  }
+
+  async function submitCode() {
+    const url = `http://localhost:3000/submitCode/${
+      props.type == "AlgorithmRival" ? "algorithm" : "SQL"
+    }`;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({
+          userCode: props.userCode.getValue(),
+        }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        props.setOutput(data);
+        if (!openConsole) props.setOpenConsole(true);
+        props.changeToConsole();
+      } else console.log(data.message);
+    } catch (error) {
+      console.log(`Error al correr el codigo: ${error.message}`);
+    }
+  }
+
   return (
     <div className="mt-2 py-[7.5px] rounded-md flex bg-white shrink-0">
       <button
@@ -23,10 +77,20 @@ export default function ButtomBarEditor(props) {
         </div>
       </button>
       <div className="ml-auto flex items-center gap-4 mr-2">
-        <button className="py-1 font-medium items-center select-none rounded px-4 text-sm bg-slate-400">
+        <button
+          className="py-1 font-medium items-center select-none rounded px-4 text-sm bg-slate-400"
+          onClick={() => {
+            runCode();
+          }}
+        >
           Run
         </button>
-        <button className="py-1 font-medium items-center select-none rounded px-4 text-sm bg-green-500 text-white">
+        <button
+          className="py-1 font-medium items-center select-none rounded px-4 text-sm bg-green-500 text-white"
+          onClick={() => {
+            submitCode();
+          }}
+        >
           Submit
         </button>
       </div>
