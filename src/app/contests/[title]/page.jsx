57f9fe350d136720e694824f7a4fb86ced66ecce
carlos paez <carlosa.paezc@autonoma.edu.co>
2023-11-05 16:55:52 -0500

    a

--- src/app/contests/[title]/page.jsx
+++ src/app/contests/[title]/page.jsx
@@ -1,12 +1,17 @@
 "use client";
 import dynamic from "next/dynamic";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
 import { ScrollPanel } from "primereact/scrollpanel";
 import { useState, useEffect } from "react";
 const ShowMD = dynamic(() => import("@/components/ShowMD"), {
   ssr: false,
 });
-async function getContest(name) {
-  const url = `http://localhost:3000/api/v1/contests/${name}`;
+async function getContest(title) {
+  const url = `http://localhost:3000/api/v1/contests/${title.replace(
+    "-",
+    " ",
+  )}`;
 
   try {
     const response = await fetch(url, {
@@ -25,6 +30,7 @@ async function getContest(name) {
 export default function Rival({ params }) {
   const [contest, setContest] = useState({});
   const [loadingContest, setLoadingContest] = useState(true);
+  const [selectedRival, setSelectedRival] = useState({});
 
   useEffect(() => {
     async function fetchRival() {
@@ -36,19 +42,74 @@ export default function Rival({ params }) {
     fetchRival();
   }, [params.title]);
 
+  const difficultyBodyTemplate = (rival) => {
+    let color;
+    if (rival.difficulty === "Easy") color = "text-[#00b8a3]";
+    else if (rival.difficulty === "Medium") color = "text-[#FFBE1C]";
+    else color = "text-[#ef4444]";
+    return <h1 className={`${color} font-bold`}>{rival.difficulty}</h1>;
+  };
+
+  const tittleBodyTemplate = (rival) => {
+    return (
+      <button
+        className="font-medium hover:text-primary-400"
+        onClick={() => {
+          setSelectedRival(rival);
+        }}
+      >
+        {rival.title}
+      </button>
+    );
+  };
+
   return (
-    <main className="flex flex-col border border-solid border-gray-300 m-4 p-4 rounded-lg gap-4">
+    <main className="flex flex-col border border-solid border-gray-300 m-4 p-4 rounded-lg gap-4 h-[90vh]">
       <section className="p-2">
-        <h1 className="text-black text-5xl">{contest?.title}</h1>
+        <h1 className="text-black font-bold text-5xl">{contest?.title}</h1>
       </section>
-      <ScrollPanel pt={{ barY: "bg-primary-200" }}>
-        <section className="border border-solid border-gray-500 rounded-lg">
-          <ShowMD markdown={contest?.description} />
+      <ScrollPanel className="h-[45%]" pt={{ barY: "bg-primary-200" }}>
+        <section className="h-full border border-solid border-gray-300 rounded-lg">
+          {!loadingContest && <ShowMD markdown={contest?.description} />}
         </section>
       </ScrollPanel>
-      <section className="grid grid-cols-5">
-        <section className="col-span-3"></section>
-        <section className=""></section>
+      <section className="h-[45%] lg:flex lg:gap-5 border border-solid border-gray-300 rounded-lg">
+        <ScrollPanel
+          className="p-4 hidden lg:block w-[55%] lg:col-span-3 border-r boder-solid border-gray-300 rounded-lg"
+          pt={{ barY: "bg-primary-200" }}
+        >
+          <section className="overflow-y-scroll jborder border-solid border-gray-300 rounded-lg">
+            {selectedRival.statement && (
+              <ShowMD markdown={selectedRival.statement} />
+            )}
+          </section>
+        </ScrollPanel>
+        <section className="p-2 lg:border-l boder-solid border-gray-300 lg:col-span-2 rounded-lg">
+          <DataTable
+            removableSort
+            value={contest?.rivals}
+            dataKey="id"
+            paginator
+            rows={10}
+            rowsPerPageOptions={[10, 25, 50]}
+            emptyMessage="No rivals found"
+            tableStyle={{ minWidth: "50rem" }}
+          >
+            <Column
+              field="title"
+              header="Tittle"
+              sortable
+              body={tittleBodyTemplate}
+            ></Column>
+            <Column
+              field="difficulty"
+              header="Difficulty"
+              body={difficultyBodyTemplate}
+              sortable
+            ></Column>
+            <Column field="avgGrade" header="Grade" sortable></Column>
+          </DataTable>
+        </section>
       </section>
     </main>
   );
